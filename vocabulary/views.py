5f3fef39d3b80e668b308f614cc3622b4cc0b2c2
Gamattowi <p.romanczuk31@gmail.com>
2021-06-18 23:02:18 +0200

    add Django REST framemowrk

--- vocabulary/views.py
+++ vocabulary/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view(['GET'])
 def home(request):
-    return JsonResponse('Hello World!', safe=False)
+    return Response('Hello World!')
