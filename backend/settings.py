d805147f5e4209ed597b10fe29367e20d40372be
Gamattowi <p.romanczuk31@gmail.com>
2021-06-19 18:15:23 +0200

    add axios to load data from backend

--- backend/settings.py
+++ backend/settings.py
@@ -42,12 +42,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
 
     'vocabulary.apps.VocabularyConfig',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,3 +134,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- frontend/src/screens/WordsListScreen.js
+++ frontend/src/screens/WordsListScreen.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Word from "../components/Word";
+import axios from "axios";
 
 const WordsListScreen = () => {
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    async function fetchWords() {
+      const { data } = await axios.get("http://127.0.0.1:8000/words/");
+      setWords(data);
+    }
+    fetchWords();
+  }, []);
   return (
     <div>
       <h1>Words</h1>
       <Row>
-        <Col sm={12} md={6} lg={4} xl={3}>
-          <Word />
-        </Col>
+        {words.map((word) => (
+          <Col key={word.id} sm={12} md={6} lg={4} xl={3}>
+            <Word />
+          </Col>
+        ))}
       </Row>
     </div>
   );
