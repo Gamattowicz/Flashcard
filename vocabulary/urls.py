1e57bcbb5a2097a56a99140eda2801d102595231
Gamattowi <p.romanczuk31@gmail.com>
2021-06-19 17:12:33 +0200

    create get word view

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, get_words
+from .views import home, get_words, get_word
 
 urlpatterns = [
     path('', home, name='home'),
     path('words/', get_words, name='words'),
+    path('words/<str:pk>/', get_word, name='word'),
 ]
--- vocabulary/views.py
+++ vocabulary/views.py
@@ -14,3 +14,10 @@ def get_words(request):
     words = Word.objects.all()
     serializer = WordSerializer(words, many=True)
     return Response(serializer.data)
+
+
+@api_view(['GET'])
+def get_word(request, pk):
+    word = Word.objects.get(id=pk)
+    serializer = WordSerializer(word, many=False)
+    return Response(serializer.data)
