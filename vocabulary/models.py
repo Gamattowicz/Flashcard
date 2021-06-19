c9cfdf8ee5188b4f52b812615eabde69df8212a4
Gamattowi <p.romanczuk31@gmail.com>
2021-06-19 14:16:13 +0200

    create Category and Word model

--- vocabulary/models.py
+++ vocabulary/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    color = models.CharField(max_length=7, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return f'{str(self.name)}'
+
+
+class Word(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    definition = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{str(self.name)}'
+
+    class Meta:
+        ordering = ['name']
