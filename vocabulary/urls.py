b2d84142a961330e7a56865f55530c43f8381cde
Gamattowi <p.romanczuk31@gmail.com>
2021-06-20 22:37:08 +0200

    add JSON Web Token

--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import home, get_words, get_word, get_categories, get_category
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
+    path('api/users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', home, name='home'),
     path('words/', get_words, name='words'),
     path('words/<str:pk>/', get_word, name='word'),
