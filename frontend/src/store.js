e8deb8744aadb2381278f1bf5ca6731082e08346
Gamattowi <p.romanczuk31@gmail.com>
2021-06-21 18:22:50 +0200

    create user register reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,13 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { wordListReducer, wordDetailsReducer } from "./reducers/wordReducers";
-import { userLoginReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   wordList: wordListReducer,
   wordDetails: wordDetailsReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const userInfoFromStorage = localStorage.getItem("userInfo")
