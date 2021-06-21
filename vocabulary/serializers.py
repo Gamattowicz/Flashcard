b24dc1ff63631ba019c2b599bca0f4769c6ed708
Gamattowi <p.romanczuk31@gmail.com>
2021-06-21 13:08:44 +0200

    create users serialiers

--- vocabulary/serializers.py
+++ vocabulary/serializers.py
@@ -1,37 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
-from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Word, Category
-
-
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField(read_only=True)
-    isAdmin = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'name', 'isAdmin']
-
-    def get_isAdmin(self, obj):
-        return obj.is_staff
-
-    def get_name(self, obj):
-        name = obj.first_name
-        if name == '':
-            name = obj.email
-        return name
-
-
-class UserSerializerWithToken(UserSerializer):
-    token = serializers.SerializerMethodField(read_only=True)
-
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email', 'name', 'isAdmin', 'token']
-
-    def get_token(self, obj):
-        token = RefreshToken.for_user(obj)
-        return str(token.access_token)
+from .models import Word
 
 
 class WordSerializer(serializers.ModelSerializer):
@@ -43,9 +11,3 @@ class WordSerializer(serializers.ModelSerializer):
         rep = super(WordSerializer, self).to_representation(instance)
         rep['category'] = instance.category.name
         return rep
-
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = '__all__'
--- vocabulary/urls.py
+++ vocabulary/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from .views import get_words, get_word, MyTokenObtainPairView, get_user_profile, \
-    get_users, register_user
+from .views import get_words, get_word
 
 urlpatterns = [
-    path('api/users/login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/users/register/', register_user, name='register'),
-    path('users/profile/', get_user_profile, name='user_profile'),
-    path('users/', get_users, name='users'),
     path('', get_words, name='words'),
     path('<str:pk>/', get_word, name='word'),
 ]
