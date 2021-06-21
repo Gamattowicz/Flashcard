a2f80d415017b713e2480d99ffe8d7335e80f4da
Gamattowi <p.romanczuk31@gmail.com>
2021-06-21 18:26:34 +0200

    create register action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 import axios from "axios";
 
@@ -43,4 +46,37 @@ export const logout = () => (dispatch) => {
   dispatch({
     type: USER_LOGOUT,
   });
+};
+
+export const register = (username, email, password) => async (dispatch) => {
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+      },
+    };
+    const { data } = await axios.post(
+      "/users/register/",
+      {
+        username: username,
+        password: password,
+        email: email,
+      },
+      config
+    );
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
 };
