f42e24d3405148c4b6f36ee1db80db0bc5637617
Gamattowi <p.romanczuk31@gmail.com>
2021-06-22 20:20:16 +0200

    add create word view

--- words/urls.py
+++ words/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import get_words, get_word
+from .views import get_words, get_word, create_word
 
 urlpatterns = [
     path('', get_words, name='words'),
     path('<str:pk>/', get_word, name='word'),
+    path('<str:pk>/create/', create_word, name='word_create'),
 ]
--- words/views.py
+++ words/views.py
@@ -1,6 +1,8 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Word
+from category.models import Category
 from .serializers import WordSerializer
 
 
@@ -14,5 +16,23 @@ def get_words(request):
 @api_view(['GET'])
 def get_word(request, pk):
     word = Word.objects.get(id=pk)
+    serializer = WordSerializer(word, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_word(request, pk):
+    user = request.user
+    category = Category.objects.get(id=pk)
+    data = request.data
+
+    word = Word.objects.create(
+        user=user,
+        name=data['name'],
+        definition=data['definition'],
+        category=category,
+    )
+
     serializer = WordSerializer(word, many=False)
     return Response(serializer.data)
