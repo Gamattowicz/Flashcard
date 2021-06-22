d55649b902546427898efb550eb869f2c8171e61
Gamattowi <p.romanczuk31@gmail.com>
2021-06-22 22:47:07 +0200

    create category component

---
+++ frontend/src/components/Category.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const Category = ({ category }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <Link to={`/category/${category.id}`}>{category.name}</Link>
+    </Card>
+  );
+};
+
+export default Category;
