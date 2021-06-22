0e6cdb4e4af5257465d0a45f753cc1633424ef0e
Gamattowi <p.romanczuk31@gmail.com>
2021-06-22 18:51:34 +0200

    add create category view

--- category/urls.py
+++ category/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
-from .views import get_categories, get_category
+from .views import get_categories, get_category, create_category
 
 urlpatterns = [
     path('', get_categories, name='categories'),
+    path('create/', create_category, name='category_create'),
     path('<str:pk>/', get_category, name='category'),
 ]
--- category/views.py
+++ category/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -14,5 +15,19 @@ def get_categories(request):
 @api_view(['GET'])
 def get_category(request, pk):
     category = Category.objects.get(id=pk)
+    serializer = CategorySerializer(category, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_category(request):
+    data = request.data
+
+    category = Category.objects.create(
+        name=data['name'],
+        color=data['color'],
+    )
+
     serializer = CategorySerializer(category, many=False)
     return Response(serializer.data)
