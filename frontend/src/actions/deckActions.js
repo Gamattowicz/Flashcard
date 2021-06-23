317a4c192c7d5330783401af512c09c9f5370870
Gamattowi <p.romanczuk31@gmail.com>
2021-06-23 22:01:57 +0200

    create list decks

---
+++ frontend/src/actions/deckActions.js
@@ -0,0 +1,24 @@
+import {
+  DECK_LIST_REQUEST,
+  DECK_LIST_SUCCESS,
+  DECK_LIST_FAIL,
+} from "../constants/deckConstants";
+
+export const listDecks = () => async (dispatch) => {
+  try {
+    dispatch({ type: DECK_LIST_REQUEST });
+    const { data } = await axios.get("/decks/");
+    dispatch({
+      type: DECK_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: DECK_LIST_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
