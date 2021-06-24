a18ed392f378548d5be5ed4096a44c013947abc9
Gamattowi <p.romanczuk31@gmail.com>
2021-06-24 18:14:10 +0200

    create deck serializer

--- decks/views.py
+++ decks/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Deck
+from .serializers import DeckSerialzer
 # Create your views here.
