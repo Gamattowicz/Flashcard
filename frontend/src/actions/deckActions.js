5e6d87069c609503cd250a6a29626125aeb7f043
Gamattowi <p.romanczuk31@gmail.com>
2021-06-24 22:23:49 +0200

    add create deck action

--- frontend/src/actions/deckActions.js
+++ frontend/src/actions/deckActions.js
@@ -6,6 +6,9 @@ import {
   DECK_DETAILS_REQUEST,
   DECK_DETAILS_SUCCESS,
   DECK_DETAILS_FAIL,
+  DECK_CREATE_REQUEST,
+  DECK_CREATE_SUCCESS,
+  DECK_CREATE_FAIL,
 } from "../constants/deckConstants";
 
 export const listDecks = () => async (dispatch) => {
@@ -30,7 +33,9 @@ export const listDecks = () => async (dispatch) => {
 export const listDeckDetails = (id) => async (dispatch) => {
   try {
     dispatch({ type: DECK_DETAILS_REQUEST });
+
     const { data } = await axios.get(`/decks/${id}`);
+
     dispatch({
       type: DECK_DETAILS_SUCCESS,
       payload: data,
@@ -40,6 +45,38 @@ export const listDeckDetails = (id) => async (dispatch) => {
       type: DECK_DETAILS_FAIL,
       payload:
         error.reponse && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const createDeck = (name) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: DECK_CREATE_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.post("/decks/create/", { name }, config);
+
+    dispatch({
+      type: DECK_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: DECK_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.detail
           ? error.response.data.detail
           : error.message,
     });
