5072ed9cd40ebb03bcc7b9e7785e072ce1fc4277
Gamattowi <p.romanczuk31@gmail.com>
2021-06-24 20:58:12 +0200

    create get decks view

--- decks/urls.py
+++ decks/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import get_decks
 
 
 urlpatterns = [
+    path('', get_decks, name='decks'),
 ]
--- decks/views.py
+++ decks/views.py
@@ -2,5 +2,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from .models import Deck
-from .serializers import DeckSerialzer
-# Create your views here.
+from .serializers import DeckSerializer
+
+
+@api_view(['GET'])
+def get_decks(request):
+    decks = Deck.objects.all()
+    serializer = DeckSerializer(decks, many=True)
+    return Response(serializer.data)
