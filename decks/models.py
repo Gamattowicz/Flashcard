a37ff8ffa6350665b7d352ca50bcb426f89a9078
Gamattowi <p.romanczuk31@gmail.com>
2021-06-24 18:05:46 +0200

    create deck model

--- decks/models.py
+++ decks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Deck(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{str(self.name)}'
