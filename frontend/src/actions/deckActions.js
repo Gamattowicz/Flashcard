6edef2eb829f927a88d8818bd25e98c8b191948e
Gamattowi <p.romanczuk31@gmail.com>
2021-06-24 21:53:19 +0200

    create list deck details action

--- frontend/src/actions/deckActions.js
+++ frontend/src/actions/deckActions.js
@@ -3,6 +3,9 @@ import {
   DECK_LIST_REQUEST,
   DECK_LIST_SUCCESS,
   DECK_LIST_FAIL,
+  DECK_DETAILS_REQUEST,
+  DECK_DETAILS_SUCCESS,
+  DECK_DETAILS_FAIL,
 } from "../constants/deckConstants";
 
 export const listDecks = () => async (dispatch) => {
@@ -18,6 +21,25 @@ export const listDecks = () => async (dispatch) => {
       type: DECK_LIST_FAIL,
       payload:
         error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    });
+  }
+};
+
+export const listDeckDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: DECK_DETAILS_REQUEST });
+    const { data } = await axios.get(`/decks/${id}`);
+    dispatch({
+      type: DECK_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: DECK_DETAILS_FAIL,
+      payload:
+        error.reponse && error.response.data.detail
           ? error.response.data.detail
           : error.message,
     });
