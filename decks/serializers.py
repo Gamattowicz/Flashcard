670ee600383eaa3b8b228faec004ae2df315a632
Gamattowi <p.romanczuk31@gmail.com>
2021-06-25 13:32:46 +0200

    add set of words in deck serilzer

--- decks/serializers.py
+++ decks/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Deck
+from words.models import Word
 
 
 class DeckSerializer(serializers.ModelSerializer):
+    words = serializers.SlugRelatedField(many=True, source='word_set', queryset=Word.objects.all(), slug_field='name')
+
     class Meta:
         model = Deck
-        fields = '__all__'
+        fields = ('id', 'name', 'words')
