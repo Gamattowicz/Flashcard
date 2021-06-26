205b16c26f27dab255da6a03736c0a0567b97a4f
Gamattowi <p.romanczuk31@gmail.com>
2021-06-26 22:32:32 +0200

    add create_exercise view

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import create_exercise
 
-urlpatterns = []
+urlpatterns = [
+    path('<str:pk>/create/', create_exercise, name='exercise_create'),
+]
--- exercises/views.py
+++ exercises/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Exercise
+from .serializers import ExerciseSerializer
+from decks.models import Deck
 
-# Create your views here.
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_exercise(request, pk):
+    user = request.user
+    deck = Deck.objects.get(id=pk)
+    data = request.data
+
+    exercise = Exercise.objects.create(
+        user=user,
+        words_num=data['words_num'],
+        correct_answers=data['correct_answers'],
+        wrong_answers=data['wrong_answers'],
+        deck=deck
+    )
+
+    serializer = ExerciseSerializer(exercise, many=False)
+    return Response(serializer.data)
