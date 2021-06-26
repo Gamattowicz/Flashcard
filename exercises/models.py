c00c789bb770b0e977374c11c28a231b8ea0357b
Gamattowi <p.romanczuk31@gmail.com>
2021-06-26 22:15:19 +0200

    create exercise model

--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from decks.models import Deck
 
 # Create your models here.
+
+
+class Exercise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    words_num = models.IntegerField(null=True, blank=True, default=5)
+    correct_answers = models.IntegerField(null=True, blank=True, default=0)
+    wrong_answers = models.IntegerField(null=True, blank=True, default=0)
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
