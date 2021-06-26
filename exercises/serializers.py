68ab90b87ea7635ed87b834f67a9b5bae0cb30cc
Gamattowi <p.romanczuk31@gmail.com>
2021-06-26 22:17:08 +0200

    create exercise serializer

---
+++ exercises/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Exercise
+
+
+class ExerciseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Exercise
+        fields = '__all__'
