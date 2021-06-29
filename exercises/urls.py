72f90524a6e5e12b4a8900ec9c50b066a7ff784b
Gamattowi <p.romanczuk31@gmail.com>
2021-06-29 16:18:42 +0200

    create update_exercise view and fix updateExercise action

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import create_exercise
+from .views import create_exercise, update_exercise
 
 urlpatterns = [
     path('<str:pk>/create/', create_exercise, name='exercise_create'),
+    path('<str:pk>/update/', update_exercise, name='exercise_update'),
 ]
