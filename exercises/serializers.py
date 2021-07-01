9da70e252b21f993d18516cd559b64696bd52d97
Gamattowi <p.romanczuk31@gmail.com>
2021-07-01 13:18:58 +0200

    change representation of deck in exercise serializer

--- exercises/serializers.py
+++ exercises/serializers.py
@@ -6,3 +6,8 @@ class ExerciseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Exercise
         fields = '__all__'
+
+    def to_representation(self, instance):
+        rep = super(ExerciseSerializer, self).to_representation(instance)
+        rep['deck'] = instance.deck.name
+        return rep
