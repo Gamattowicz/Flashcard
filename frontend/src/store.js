2d29261ba282eba4d505c879fab8c936d4ecc701
Gamattowi <p.romanczuk31@gmail.com>
2021-07-02 21:57:17 +0200

    create userList reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import {
   wordListReducer,
   wordDetailsReducer,
@@ -9,23 +9,24 @@ import {
   wordAddExerciseReducer,
   wordAddCorrectAnswerReducer,
   wordAddWrongAnswerReducer,
-} from "./reducers/wordReducers";
+} from './reducers/wordReducers'
 import {
   userLoginReducer,
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
-} from "./reducers/userReducers";
+  userListReducer,
+} from './reducers/userReducers'
 import {
   categoryListReducer,
   categoryDetailsReducer,
   categoryCreateReducer,
-} from "./reducers/categoryReducers";
+} from './reducers/categoryReducers'
 import {
   deckListReducer,
   deckDetailsReducer,
   deckCreateReducer,
-} from "./reducers/deckReducers";
+} from './reducers/deckReducers'
 
 import {
   exerciseCreateReducer,
@@ -33,7 +34,7 @@ import {
   exerciseAddCorrectAnswerReducer,
   exerciseAddWrongAnswerReducer,
   exerciseDetailsReducer,
-} from "./reducers/exerciseReducers";
+} from './reducers/exerciseReducers'
 
 const reducer = combineReducers({
   wordList: wordListReducer,
@@ -47,6 +48,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   categoryList: categoryListReducer,
   categoryDetails: categoryDetailsReducer,
   categoryCreate: categoryCreateReducer,
@@ -58,22 +60,22 @@ const reducer = combineReducers({
   exerciseAddCorrectAnswer: exerciseAddCorrectAnswerReducer,
   exerciseAddWrongAnswer: exerciseAddWrongAnswerReducer,
   exerciseDetails: exerciseDetailsReducer,
-});
+})
 
-const userInfoFromStorage = localStorage.getItem("userInfo")
-  ? JSON.parse(localStorage.getItem("userInfo"))
-  : null;
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
 const initialState = {
   userLogin: { userInfo: userInfoFromStorage },
-};
+}
 
-const middleware = [thunk];
+const middleware = [thunk]
 
 const store = createStore(
   reducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
-);
+)
 
-export default store;
+export default store
