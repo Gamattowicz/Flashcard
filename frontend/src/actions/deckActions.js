94780d697e6771ec8f5ae8142ffd151f4dc79d10
Gamattowi <p.romanczuk31@gmail.com>
2021-07-02 19:31:39 +0200

    fix typo in listDeckDetails action

--- frontend/src/actions/deckActions.js
+++ frontend/src/actions/deckActions.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios'
 import {
   DECK_LIST_REQUEST,
   DECK_LIST_SUCCESS,
@@ -9,16 +9,16 @@ import {
   DECK_CREATE_REQUEST,
   DECK_CREATE_SUCCESS,
   DECK_CREATE_FAIL,
-} from "../constants/deckConstants";
+} from '../constants/deckConstants'
 
 export const listDecks = () => async (dispatch) => {
   try {
-    dispatch({ type: DECK_LIST_REQUEST });
-    const { data } = await axios.get("/decks/");
+    dispatch({ type: DECK_LIST_REQUEST })
+    const { data } = await axios.get('/decks/')
     dispatch({
       type: DECK_LIST_SUCCESS,
       payload: data,
-    });
+    })
   } catch (error) {
     dispatch({
       type: DECK_LIST_FAIL,
@@ -26,52 +26,52 @@ export const listDecks = () => async (dispatch) => {
         error.response && error.response.data.detail
           ? error.response.data.detail
           : error.message,
-    });
+    })
   }
-};
+}
 
 export const listDeckDetails = (id) => async (dispatch) => {
   try {
-    dispatch({ type: DECK_DETAILS_REQUEST });
+    dispatch({ type: DECK_DETAILS_REQUEST })
 
-    const { data } = await axios.get(`/decks/${id}`);
+    const { data } = await axios.get(`/decks/${id}`)
 
     dispatch({
       type: DECK_DETAILS_SUCCESS,
       payload: data,
-    });
+    })
   } catch (error) {
     dispatch({
       type: DECK_DETAILS_FAIL,
       payload:
-        error.reponse && error.response.data.detail
+        error.response && error.response.data.detail
           ? error.response.data.detail
           : error.message,
-    });
+    })
   }
-};
+}
 
 export const createDeck = (name) => async (dispatch, getState) => {
   try {
-    dispatch({ type: DECK_CREATE_REQUEST });
+    dispatch({ type: DECK_CREATE_REQUEST })
 
     const {
       userLogin: { userInfo },
-    } = getState();
+    } = getState()
 
     const config = {
       headers: {
-        "Content-type": "application/json",
+        'Content-type': 'application/json',
         Authorization: `Bearer ${userInfo.token}`,
       },
-    };
+    }
 
-    const { data } = await axios.post("/decks/create/", { name }, config);
+    const { data } = await axios.post('/decks/create/', { name }, config)
 
     dispatch({
       type: DECK_CREATE_SUCCESS,
       payload: data,
-    });
+    })
   } catch (error) {
     dispatch({
       type: DECK_CREATE_FAIL,
@@ -79,6 +79,6 @@ export const createDeck = (name) => async (dispatch, getState) => {
         error.response && error.response.data.detail
           ? error.response.data.detail
           : error.message,
-    });
+    })
   }
-};
+}
