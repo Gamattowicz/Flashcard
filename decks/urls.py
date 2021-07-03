4f9e54edb80692e16ce20c6f1ef25b8d43077089
Gamattowi <p.romanczuk31@gmail.com>
2021-07-03 10:04:10 +0200

    create get_all_decks view

--- decks/urls.py
+++ decks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import get_decks, get_deck, create_deck
+from .views import get_decks, get_deck, create_deck, get_all_decks
 
 
 urlpatterns = [
     path('', get_decks, name='decks'),
+    path('admin/', get_all_decks, name='all_decks'),
     path('create/', create_deck, name='deck_create'),
     path('<str:pk>/', get_deck, name='deck'),
 ]
--- decks/views.py
+++ decks/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import Deck
 from .serializers import DeckSerializer
 
@@ -10,6 +10,14 @@ from .serializers import DeckSerializer
 def get_decks(request):
     user = request.user
     decks = Deck.objects.filter(user=user.id)
+    serializer = DeckSerializer(decks, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+@permission_classes([IsAdminUser])
+def get_all_decks(request):
+    decks = Deck.objects.all()
     serializer = DeckSerializer(decks, many=True)
     return Response(serializer.data)
 
