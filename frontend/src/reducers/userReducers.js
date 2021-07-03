97233aec060f0ff53f65d5765a5e0f9a03409850
Gamattowi <p.romanczuk31@gmail.com>
2021-07-03 10:31:51 +0200

    fix userList reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -98,7 +98,7 @@ export const userUpdateProfileReducer = (state = {}, action) => {
 export const userListReducer = (state = { users: [] }, action) => {
   switch (action.type) {
     case USER_LIST_REQUEST:
-      return { loading: true }
+      return { loading: true, users: [] }
 
     case USER_LIST_SUCCESS:
       return { loading: false, success: true, users: action.payload }
