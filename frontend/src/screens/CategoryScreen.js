0d7a442ce8bb22356a27b7f426ea1355c0d02e13
Gamattowi <p.romanczuk31@gmail.com>
2021-09-11 13:55:31 +0200

    style CategoryScreen

--- frontend/src/screens/CategoryScreen.js
+++ frontend/src/screens/CategoryScreen.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { Row, Col, ListGroup, Card } from "react-bootstrap";
-import { listCategoryDetails } from "../actions/categoryActions";
-import Loader from "../components/Loader";
-import Message from "../components/Message";
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { Row, Col, ListGroup, Card } from 'react-bootstrap'
+import { listCategoryDetails } from '../actions/categoryActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const CategoryScreen = ({ match }) => {
-  const dispatch = useDispatch();
-  const categoryDetails = useSelector((state) => state.categoryDetails);
-  const { error, loading, category } = categoryDetails;
+  const dispatch = useDispatch()
+  const categoryDetails = useSelector((state) => state.categoryDetails)
+  const { error, loading, category } = categoryDetails
 
   useEffect(() => {
-    dispatch(listCategoryDetails(match.params.id));
-  }, [dispatch]);
+    dispatch(listCategoryDetails(match.params.id))
+  }, [dispatch])
 
   return (
     <div>
@@ -25,24 +25,30 @@ const CategoryScreen = ({ match }) => {
       ) : error ? (
         <Message variant="danger">{error}</Message>
       ) : (
-        <Col md={3}>
+        <Col md={4}>
           <Card>
             <ListGroup variant="flush">
               <ListGroup.Item>
                 <Row>
-                  <Col>
+                  <Col md={5} className="align-self-center">
                     <strong>Name</strong>
                   </Col>
-                  <Col>{category.name}</Col>
+                  <Col md={7} className="text-center">
+                    {category.name}
+                  </Col>
                 </Row>
               </ListGroup.Item>
 
               <ListGroup.Item>
                 <Row>
-                  <Col>
+                  <Col md={5} className="align-self-center">
                     <strong>Color</strong>
                   </Col>
-                  <Col>{category.color}</Col>
+                  <Col
+                    md={7}
+                    className="text-center"
+                    style={{ backgroundColor: category.color }}
+                  ></Col>
                 </Row>
               </ListGroup.Item>
             </ListGroup>
@@ -50,7 +56,7 @@ const CategoryScreen = ({ match }) => {
         </Col>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CategoryScreen;
+export default CategoryScreen
