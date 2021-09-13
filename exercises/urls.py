6244aa26684f285e27e863ec7bfacf92b1774afc
Gamattowi <p.romanczuk31@gmail.com>
2021-09-13 19:30:37 +0200

    change FBV exercises views to CBV

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import create_exercise, add_correct_answer, add_wrong_answer, get_exercises, get_exercise, get_all_exercises
+from .views import ExerciseList, ExerciseAllList, ExerciseDetail, ExerciseCreate, ExerciseUpdateCorrectAnswer, ExerciseUpdateWrongAnswer
 
 urlpatterns = [
-    path('<str:pk>/create/', create_exercise, name='exercise_create'),
-    path('<str:pk>/correct-answer/', add_correct_answer, name='add_correct_answer'),
-    path('<str:pk>/wrong-answer/', add_wrong_answer, name='add_wrong_answer'),
-    path('', get_exercises, name='exercises'),
-    path('admin/', get_all_exercises, name='all_exercises'),
-    path('<str:pk>/', get_exercise, name='exercise'),
+    path('<str:pk>/create/', ExerciseCreate.as_view(), name='exercise_create'),
+    path('<str:pk>/correct-answer/', ExerciseUpdateCorrectAnswer.as_view(), name='add_correct_answer'),
+    path('<str:pk>/wrong-answer/', ExerciseUpdateWrongAnswer.as_view(), name='add_wrong_answer'),
+    path('', ExerciseList.as_view(), name='exercises'),
+    path('admin/', ExerciseAllList.as_view(), name='all_exercises'),
+    path('<str:pk>/', ExerciseDetail.as_view(), name='exercise'),
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,99 +1,115 @@
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView
 from .models import Exercise
 from .serializers import ExerciseSerializer
 from decks.models import Deck
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def create_exercise(request, pk):
-    user = request.user
-    deck = Deck.objects.get(id=pk)
-    data = request.data
+class ExerciseList(ListAPIView):
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
 
-    exercise = Exercise.objects.create(
-        user=user,
-        words_num=data['wordNumber'],
-        correct_answers=data['correctAnswers'],
-        wrong_answers=data['wrongAnswers'],
-        deck=deck
-    )
+    def list(self, request, *args, **kwargs):
+        user = request.user
+        queryset = Exercise.objects.filter(user=user.id).order_by('created_at')
 
-    serializer = ExerciseSerializer(exercise, many=False)
-    return Response(serializer.data)
+        page = request.query_params.get('page')
+        paginator = Paginator(queryset, 2)
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            queryset = paginator.page(1)
+        except EmptyPage:
+            queryset = paginator.page(paginator.num_pages)
+        if page is None:
+            page = 1
+        page = int(page)
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_exercises(request):
-    user = request.user
-    exercises = Exercise.objects.filter(user=user.id)
-
-    page = request.query_params.get('page')
-    paginator = Paginator(exercises, 12)
-    try:
-        exercises = paginator.page(page)
-    except PageNotAnInteger:
-        exercises = paginator.page(1)
-    except EmptyPage:
-        exercises = paginator.page(paginator.num_pages)
-    if page is None:
-        page = 1
-    page = int(page)
-
-    serializer = ExerciseSerializer(exercises, many=True)
-    return Response({'exercises': serializer.data, 'page': page, 'pages': paginator.num_pages})
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'exercises': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 
-@api_view(['GET'])
-@permission_classes([IsAdminUser])
-def get_all_exercises(request):
-    exercises = Exercise.objects.all()
+class ExerciseAllList(ListAPIView):
+    queryset = Exercise.objects.all().order_by('created_at')
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAdminUser]
 
-    page = request.query_params.get('page')
-    paginator = Paginator(exercises, 12)
-    try:
-        exercises = paginator.page(page)
-    except PageNotAnInteger:
-        exercises = paginator.page(1)
-    except EmptyPage:
-        exercises = paginator.page(paginator.num_pages)
-    if page is None:
-        page = 1
-    page = int(page)
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
 
-    serializer = ExerciseSerializer(exercises, many=True)
-    return Response({'exercises': serializer.data, 'page': page, 'pages': paginator.num_pages})
+        page = request.query_params.get('page')
+        paginator = Paginator(queryset, 2)
+        try:
+            queryset = paginator.page(page)
+        except PageNotAnInteger:
+            queryset = paginator.page(1)
+        except EmptyPage:
+            queryset = paginator.page(paginator.num_pages)
+        if page is None:
+            page = 1
+        page = int(page)
 
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_exercise(request, pk):
-    exercises = Exercise.objects.get(id=pk)
-    serializer = ExerciseSerializer(exercises, many=False)
-    return Response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({'exercises': serializer.data, 'page': page, 'pages': paginator.num_pages})
 
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def add_correct_answer(request, pk):
-    exercise = Exercise.objects.get(id=pk)
-    exercise.correct_answers += 1
-    exercise.save()
-
-    serializer = ExerciseSerializer(exercise, many=False)
-    return Response(serializer.data)
+class ExerciseDetail(RetrieveAPIView):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
 
 
-@api_view(['PUT'])
-@permission_classes([IsAuthenticated])
-def add_wrong_answer(request, pk):
-    exercise = Exercise.objects.get(id=pk)
-    exercise.wrong_answers += 1
-    exercise.save()
+class ExerciseCreate(CreateAPIView):
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
 
-    serializer = ExerciseSerializer(exercise, many=False)
-    return Response(serializer.data)
+    def post(self, request, pk, *args, **kwargs):
+        user = request.user
+        deck = Deck.objects.get(id=pk)
+        data = request.data
+
+        exercise = Exercise.objects.create(
+            user=user,
+            words_num=data['wordNumber'],
+            correct_answers=data['correctAnswers'],
+            wrong_answers=data['wrongAnswers'],
+            deck=deck
+        )
+
+        serializer = ExerciseSerializer(exercise, many=False)
+        return Response(serializer.data)
+
+
+class ExerciseUpdateCorrectAnswer(UpdateAPIView):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.correct_answers += 1
+        instance.save()
+        correct_answers = {'correct_answers': instance.correct_answers}
+        serializer = self.serializer_class(request.user, data=correct_answers, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+
+        return Response(serializer.data)
+
+
+class ExerciseUpdateWrongAnswer(UpdateAPIView):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.wrong_answers += 1
+        instance.save()
+        wrong_answers = {'wrong_answers': instance.wrong_answers}
+        serializer = self.serializer_class(request.user, data=wrong_answers, partial=True)
+        if serializer.is_valid():
+            self.perform_update(serializer)
+
+        return Response(serializer.data)
