1fa6d00e4dc633440beac1f9075246b113308499
Gamattowi <p.romanczuk31@gmail.com>
2021-09-17 20:18:29 +0200

    style EndExerciseScreen

--- frontend/src/screens/EndExerciseScreen.js
+++ frontend/src/screens/EndExerciseScreen.js
@@ -4,6 +4,7 @@ import { Row, Col, ListGroup, Card, Container } from 'react-bootstrap'
 import Loader from '../components/Loader'
 import Message from '../components/Message'
 import { listExerciseDetails } from '../actions/exerciseActions'
+import ExerciseChart from '../components/ExerciseChart'
 
 const EndExerciseScreen = ({ match }) => {
   const dispatch = useDispatch()
@@ -16,51 +17,58 @@ const EndExerciseScreen = ({ match }) => {
   }, [dispatch])
 
   return (
-    <div>
-      <Container>
-        <Row className="justify-content-md-center">
-          {loading ? (
-            <Loader />
-          ) : error ? (
-            <Message variant="danger">{error}</Message>
-          ) : (
-            <Col md={5}>
-              <h4>Your exercise session has ended.</h4>
-              <Card>
-                <ListGroup variant="flush">
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>
-                        <strong>Words number</strong>
-                      </Col>
-                      <Col>{exercise.words_num}</Col>
-                    </Row>
-                  </ListGroup.Item>
+    <Container>
+      <Row className="justify-content-md-center">
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Col md={5}>
+            <h4>Your exercise session has ended.</h4>
+            <Card className="mt-3">
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>
+                      <strong>Words number</strong>
+                    </Col>
+                    <Col>{exercise.words_num}</Col>
+                  </Row>
+                </ListGroup.Item>
 
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>
-                        <strong>Correct answers</strong>
-                      </Col>
-                      <Col>{exercise.correct_answers}</Col>
-                    </Row>
-                  </ListGroup.Item>
-
-                  <ListGroup.Item>
-                    <Row>
-                      <Col>
-                        <strong>Wrong number answers</strong>
-                      </Col>
-                      <Col>{exercise.wrong_answers}</Col>
-                    </Row>
-                  </ListGroup.Item>
-                </ListGroup>
-              </Card>
-            </Col>
-          )}
-        </Row>
-      </Container>
-    </div>
+                <ListGroup.Item>
+                  <Row>
+                    <Col md={2} className="align-self-center text-success">
+                      <strong>Correct answers</strong>
+                    </Col>
+                    <Col md={4} className="text-center text-success">
+                      {exercise.correct_answers}
+                    </Col>
+                    <Col md={2} className="align-self-center text-warning">
+                      <strong>Wrong answers</strong>
+                    </Col>
+                    <Col md={4} className="text-center text-warning">
+                      {exercise.wrong_answers}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+            <ExerciseChart
+              data={[
+                {
+                  name: 'GOOD ANSWERS',
+                  value: exercise.correct_answers,
+                },
+                { name: 'BAD ANSWERS', value: exercise.wrong_answers },
+              ]}
+              colors={['#00C49F', '#FF8042']}
+            />
+          </Col>
+        )}
+      </Row>
+    </Container>
   )
 }
 
