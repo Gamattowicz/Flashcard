e93e799339a862acd9d756bb852dee2a89f6071c
Gamattowi <p.romanczuk31@gmail.com>
2021-09-22 19:34:03 +0200

    create userDeleteReducer

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,12 +1,80 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Button, Card, Col, Modal, Row} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {deleteUser} from '../actions/userActions'
+import {USER_DELETE_RESET} from '../constants/userConstants'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 const User = ({ user }) => {
+  const [show, setShow] = useState(false)
+
+  const handleShow = () => setShow(true)
+  const handleClose = () => setShow(false)
+
+  const dispatch = useDispatch()
+
+  const userDelete = useSelector((state) => state.userDelete)
+  const { error, loading, success } = userDelete
+
+  useEffect(() => {
+    if (success) {
+      window.location.reload()
+      dispatch({ type: USER_DELETE_RESET })
+    }
+  }, [success])
+
+  const deleteHandler = () => {
+    dispatch(deleteUser(user.id))
+    handleClose()
+  }
+
   return (
-    <Card className="my-3 p-3 rounded">
-      <Link to={`/admin/user/${user.id}`}>{user.username}</Link>
-    </Card>
+    <>
+      {error && <Message variant="danger">{error}</Message>}
+      {loading && <Loader />}
+      <Card className="my-3 p-3 rounded">
+        <Row>
+          <Col md={8}>
+            <Link to={`/admin/user/${user.id}`}>{user.username}</Link>
+          </Col>
+          <Col md={2}>
+            <Link to={`/users/${user.id}/update`}>
+              <i class="fas fa-edit text-primary"></i>
+            </Link>
+          </Col>
+          <Col md={2}>
+            <i class="fas fa-trash text-warning" onClick={handleShow}></i>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header>
+                <Modal.Title>Delete user</Modal.Title>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  onClick={handleClose}
+                >
+                  <span aria-hidden="true"></span>
+                </button>
+              </Modal.Header>
+              <Modal.Body>
+                Are you sure you want to remove user "{user.username}"?
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                  Cancel
+                </Button>
+                <Button variant="warning" onClick={deleteHandler}>
+                  Delete user
+                </Button>
+              </Modal.Footer>
+            </Modal>
+          </Col>
+        </Row>
+      </Card>
+    </>
   )
 }
 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,22 +1,26 @@
 import {
-  USER_LOGIN_REQUEST,
-  USER_LOGIN_SUCCESS,
-  USER_LOGIN_FAIL,
-  USER_LOGOUT,
-  USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS,
-  USER_REGISTER_FAIL,
-  USER_DETAILS_REQUEST,
-  USER_DETAILS_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_RESET,
+  USER_DELETE_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
   USER_DETAILS_RESET,
-  USER_UPDATE_PROFILE_REQUEST,
-  USER_UPDATE_PROFILE_SUCCESS,
-  USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_RESET,
+  USER_DETAILS_SUCCESS,
+  USER_LIST_FAIL,
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
-  USER_LIST_FAIL,
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_UPDATE_PROFILE_FAIL,
+  USER_UPDATE_PROFILE_REQUEST,
+  USER_UPDATE_PROFILE_RESET,
+  USER_UPDATE_PROFILE_SUCCESS,
 } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -111,6 +115,27 @@ export const userListReducer = (state = { users: [] }, action) => {
 
     case USER_LIST_FAIL:
       return { loading: false, error: action.payload }
+
+    default:
+      return state
+  }
+}
+
+export const userDeleteReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DELETE_REQUEST:
+      return { loading: true }
+
+    case USER_DELETE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      }
+
+    case USER_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+    case USER_DELETE_RESET:
+      return { user: {} }
 
     default:
       return state
