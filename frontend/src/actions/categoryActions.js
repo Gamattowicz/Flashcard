ba1a8783dba00dc1c8937bcfab110755160a20b1
Gamattowi <p.romanczuk31@gmail.com>
2021-09-22 18:47:49 +0200

    create updateCategory action

--- frontend/src/actions/categoryActions.js
+++ frontend/src/actions/categoryActions.js
@@ -12,6 +12,9 @@ import {
   CATEGORY_LIST_FAIL,
   CATEGORY_LIST_REQUEST,
   CATEGORY_LIST_SUCCESS,
+  CATEGORY_UPDATE_FAIL,
+  CATEGORY_UPDATE_REQUEST,
+  CATEGORY_UPDATE_SUCCESS,
 } from '../constants/categoryConstants'
 
 export const listCategories =
@@ -117,6 +120,42 @@ export const deleteCategory = (id) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: CATEGORY_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.detail
+          ? error.response.data.detail
+          : error.message,
+    })
+  }
+}
+
+export const updateCategory = (category) => async (dispatch, getState) => {
+  try {
+    dispatch({ type: CATEGORY_UPDATE_REQUEST })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.patch(
+      `/category/${category.id}/update/`,
+      category,
+      config
+    )
+
+    dispatch({
+      type: CATEGORY_UPDATE_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_UPDATE_FAIL,
       payload:
         error.response && error.response.data.detail
           ? error.response.data.detail
